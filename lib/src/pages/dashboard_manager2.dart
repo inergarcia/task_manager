ae662a7de1e2d2a55713122f884bfb1fa6fac753
Iner Garcia Rodriguez <inergarciarodriguez@gmail.com>
2021-09-23 14:28:03 +0200

    Create pages
    AccountPage
    ActionTaskPage
    AddProgrammerTaskPage
    DashBoardPageManager1
    DashBoardPageManager2
    DashBoardPageProgrammer
    ListTaskManager1
    ListTaskManager2
    ListTaskProgrammers
    LoginPage
    NewTaskPage
    TaskDetailsPage

---
+++ lib/src/pages/dashboard_manager2.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:task_manager/src/design/colors.dart';
+import 'package:task_manager/src/design/strings.dart';
+import 'package:task_manager/src/pages/account.dart';
+import 'package:task_manager/src/pages/add_programmer_task.dart';
+import 'package:task_manager/src/pages/list_task_manager2.dart';
+import 'package:task_manager/src/providers/auth.dart';
+import 'package:task_manager/src/providers/navigator_index.dart';
+
+class DashBoardPageManager2 extends StatefulWidget {
+  const DashBoardPageManager2({Key? key}) : super(key: key);
+
+  @override
+  _DashBoardPageManager2State createState() => _DashBoardPageManager2State();
+}
+
+class _DashBoardPageManager2State extends State<DashBoardPageManager2> {
+  final List<String> titles = [
+    AppStrings.titleHome,
+    AppStrings.titleAddProgrammers,
+    AppStrings.titleAccount,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    AuthProvider authProvider = Provider.of<AuthProvider>(context);
+    NavigatorIndex navigatorIndex = Provider.of<NavigatorIndex>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(titles[navigatorIndex.selectedIndexHome]),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              child: Text(
+                authProvider.user != null
+                    ? authProvider.user!.name.substring(0, 2)
+                    : '',
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: AppColors.primaryColor,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        currentIndex: navigatorIndex.selectedIndexHome,
+        onTap: (index) => _onItemTapped(index, navigatorIndex),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+            ),
+            label: AppStrings.home,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_add_outlined,
+            ),
+            label: AppStrings.titleAddProgrammers,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_circle_outlined,
+            ),
+            label: AppStrings.account,
+          ),
+        ],
+      ),
+      body: IndexedStack(
+        index: navigatorIndex.selectedIndexHome,
+        children: [
+          ListTaskManager2(),
+          const AddProgrammerTaskPage(),
+          const AccountPage(),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index, NavigatorIndex navigatorIndex) {
+    navigatorIndex.selectedIndexHome = index;
+  }
+}
---
+++ lib/src/pages/task_details.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:task_manager/src/design/colors.dart';
+import 'package:task_manager/src/helpers/date.dart';
+import 'package:task_manager/src/providers/task.dart';
+import 'package:task_manager/src/widgets/setting_title.dart';
+
+class TaskDetailsPage extends StatelessWidget {
+  const TaskDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TaskProvider taskProvider = Provider.of(context, listen: false);
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Task Details'),
+        leading: IconButton(
+          onPressed: () async {
+            Navigator.pop(context);
+            await Future.delayed(const Duration(microseconds: 1000));
+            taskProvider.selectedTask = null;
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+        ),
+      ),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          SettingTitle(
+            text: 'TITLE',
+            items: [
+              ListTile(
+                minLeadingWidth: 0,
+                tileColor: Colors.white,
+                title: Text(taskProvider.selectedTask?.title ?? ''),
+              ),
+            ],
+          ),
+          SettingTitle(
+            text: 'AUTHOR',
+            items: [
+              ListTile(
+                minLeadingWidth: 0,
+                tileColor: Colors.white,
+                title: Text(taskProvider.selectedTask!.author.name),
+              ),
+            ],
+          ),
+          SettingTitle(
+            text: 'DATE',
+            items: [
+              ListTile(
+                minLeadingWidth: 0,
+                tileColor: Colors.white,
+                title: Text(CustomDate.parseMDY(
+                    taskProvider.selectedTask!.complianceDate)),
+              ),
+            ],
+          ),
+          SettingTitle(
+            text: 'DESCRIPTION',
+            items: [
+              ListTile(
+                minLeadingWidth: 0,
+                tileColor: Colors.white,
+                title: Text(
+                  taskProvider.selectedTask!.description,
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+            ],
+          ),
+          SettingTitle(
+            text: 'TYPE',
+            items: [
+              ListTile(
+                minLeadingWidth: 0,
+                tileColor: Colors.white,
+                title: Text(
+                  taskProvider.selectedTask!.type,
+                ),
+              ),
+            ],
+          ),
+          SettingTitle(
+            text: 'PROJECT',
+            items: [
+              ListTile(
+                minLeadingWidth: 0,
+                tileColor: Colors.white,
+                title: Text(
+                  taskProvider.selectedTask!.project,
+                ),
+              ),
+            ],
+          ),
+          SettingTitle(
+            text: 'STATE',
+            items: [
+              ListTile(
+                minLeadingWidth: 0,
+                tileColor: Colors.white,
+                title: Text(
+                  taskProvider.selectedTask!.state,
+                ),
+              ),
+            ],
+          ),
+          SettingTitle(
+            text: 'RESPONSABLE',
+            items: [
+              ListTile(
+                minLeadingWidth: 0,
+                tileColor: Colors.white,
+                title: Text(
+                  taskProvider.selectedTask!.responsable.name,
+                ),
+              ),
+            ],
+          ),
+          SettingTitle(
+            text: 'PROGRAMMERS',
+            items: [
+              ListTile(
+                minLeadingWidth: 0,
+                tileColor: Colors.white,
+                title: taskProvider.selectedTask!.users.isNotEmpty
+                    ? Wrap(
+                        children: [
+                          ...List.generate(
+                            taskProvider.selectedTask!.users.length,
+                            (index) => Container(
+                              margin: const EdgeInsets.all(3),
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                color: AppColors.primaryColor,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Text(
+                                  taskProvider.selectedTask!.users[index].name,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                  )),
+                            ),
+                          ),
+                        ],
+                      )
+                    : const Text('Not yet assigned'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
