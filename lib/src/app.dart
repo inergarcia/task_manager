54bb39df580dea0999a8cfe40e7412c7beec037d
Iner Garcia Rodriguez <inergarciarodriguez@gmail.com>
2021-09-23 14:19:48 +0200

    Install and configure dependencies

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,28 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:task_manager/src/design/colors.dart';
-import 'package:task_manager/src/design/routes.dart';
+import 'package:task_manager/src/providers/data.dart';
+import 'package:task_manager/src/providers/navigator_index.dart';
+import 'package:task_manager/src/providers/notifications.dart';
+import 'package:task_manager/src/providers/task.dart';
+import 'package:task_manager/src/routes/routes.dart';
 import 'package:task_manager/src/design/strings.dart';
+import 'package:task_manager/src/providers/auth.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: AppStrings.titleApp,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: AppColors.primaryColor,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => DataProvider(),
         ),
-        hintColor: Colors.black,
-        primaryColor: AppColors.primaryColor,
-        scaffoldBackgroundColor: AppColors.background,
-        fontFamily: "OpenSans",
+        ChangeNotifierProvider(
+          create: (_) => AuthProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => NavigatorIndex(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => TaskProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => NotificationsProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: AppStrings.titleApp,
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            backgroundColor: AppColors.primaryColor,
+          ),
+          hintColor: Colors.black,
+          primaryColor: AppColors.primaryColor,
+          scaffoldBackgroundColor: AppColors.background,
+          fontFamily: "OpenSans",
+        ),
+        initialRoute: 'login',
+        routes: appRoutes,
       ),
-      initialRoute: 'login',
-      routes: appRoutes,
     );
   }
 }
