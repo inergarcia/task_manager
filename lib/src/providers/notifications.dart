15ab77799f6bceb4c59eb759382f3df913ed53ed
Iner Garcia Rodriguez <inergarciarodriguez@gmail.com>
2021-09-23 14:26:30 +0200

    Create providers
    TaskProvider
    NotificationsProvider
    NavigatorIndex
    DataProvider
    AuthProvider

---
+++ lib/src/providers/notifications.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationsProvider with ChangeNotifier {
+  late FlutterLocalNotificationsPlugin _flp;
+
+  void init() {
+    _flp = FlutterLocalNotificationsPlugin();
+    var android = const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iOS = const IOSInitializationSettings();
+    var initSetttings = InitializationSettings(android: android, iOS: iOS);
+    _flp.initialize(initSetttings);
+    notifyListeners();
+  }
+
+  Future<void> showNotification(
+      {required String title, required String msg}) async {
+    var android = const AndroidNotificationDetails(
+        'channel id', 'channel NAME', 'CHANNEL DESCRIPTION',
+        priority: Priority.high, importance: Importance.max);
+    var iOS = const IOSNotificationDetails();
+
+    var platform = NotificationDetails(android: android, iOS: iOS);
+
+    await _flp.show(0, title, msg, platform);
+  }
+}
